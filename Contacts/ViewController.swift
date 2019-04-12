18f77a4b97c68feb9db2160a3fc7df776a96cdb1
Colin Smith <devcolin@icloud.com>
2019-04-12 09:26:06 -0600

    Initialize

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Colin Smith on 4/12/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
